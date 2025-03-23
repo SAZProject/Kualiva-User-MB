c15b00378ed2b9612ab0794490250053249f0ffb
Rucci <d.rucci.2001@gmail.com>
2025-03-24 02:48:17 +0700

    remove import

--- lib/splash/splash_screen.dart
+++ lib/splash/splash_screen.dart
@@ -2,13 +2,10 @@ import 'package:bounce/bounce.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:kualiva/_data/enum/fnb_action_enum.dart';
 import 'package:kualiva/auth/bloc/auth_bloc.dart';
 import 'package:kualiva/common/app_export.dart';
-import 'package:kualiva/common/feature/current_location/current_location_bloc.dart';
 import 'package:kualiva/common/utility/check_permission.dart';
 import 'package:kualiva/main_hive.dart';
-import 'package:kualiva/places/fnb/argument/fnb_action_argument.dart';
 // import 'package:kualiva/common/utility/video_constant.dart';
 // import 'package:video_player/video_player.dart';
 

ddf046f34e7d83e738c9266dc0fa20d306d8017f
Rucci <d.rucci.2001@gmail.com>
2025-03-24 00:00:35 +0700

    Buatkan seperti di figma

---
+++ lib/places/fnb/feature/fnb_action_feature.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class FnbActionFeature extends StatefulWidget {
+  const FnbActionFeature({super.key});
+
+  @override
+  State<FnbActionFeature> createState() => _FnbActionFeatureState();
+}
+
+class _FnbActionFeatureState extends State<FnbActionFeature> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
